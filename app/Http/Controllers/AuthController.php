85efe4e4d74119c2b8764b89340384a5c39298aa
= <=>
2015-11-15 16:29:34 +0200

    Added: Login and Logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller {
+
+	private function validation($inputs){
+		
+	}
+
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function login()
+	{
+		$inputs = Request::all();
+
+		
+
+	}
+
+}
