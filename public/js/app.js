fbd0f9e53776c3bdbed3073023b0b03a0f986b13
Ali Hassan <alihassan19393@gmail.com>
2015-11-19 17:13:24 +0200

    Editing an organizer done

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,24 @@
-var app = angular.module("organizers" , ["ngRoute"]);
+var app = angular.module("organizers" , ["ngRoute"])
+    .factory("UserToEdit" , function(){
+        return {
+            data: null,
+            setData: function(obj){
+                this.data = obj;
+            }
+        };
+    });
 
 var icom = {
     fileBase: ""
 };
+
+function filter(arr , fun){
+    var f = [];
+    for(var i = 0 ; i < arr.length; i++){
+        if(fun(arr[i])){
+            f.push(arr[i]);
+        }
+    }
+    
+    return f;
+}
