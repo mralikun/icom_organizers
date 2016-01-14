d9dee1a1bdf6f222659e564875a56215bfbd3665
Ali Hassan <alihassan19393@gmail.com>
2016-01-14 17:46:20 +0200

    refactored the conference export

--- public/js/controllers/ExportController.js
+++ public/js/controllers/ExportController.js
@@ -14,29 +14,13 @@ app.controller("ExportController" , ["$scope" , "Patcher" , "$rootScope" , funct
         console.log("DONE!");
     });
     
-    function date_formater(d){
-        var day = d.getDate();
-        var month = d.getMonth();
-        var year = d.getFullYear();
-        return year + "-" + ((month < 10) ? "0"+month : month) + "-" + ((day < 10) ? "0"+day : day);
-    }
-    
-    scope.grading_conferences = function(){
-        var start = (scope.start) ? $.datepicker.parseDate("dd-mm-yy" , scope.start) : "";
-        var end = (scope.end) ? $.datepicker.parseDate("dd-mm-yy" , scope.end) : "";
-        if(start instanceof Date && end instanceof Date){
-            if(end < start){
-                scope.wrong_dates = true;
-                return false;
-            }else{
-                scope.wrong_dates = false;
-                var params = "from="+date_formater(start)+"&to="+date_formater(end);
-                network.set("url" , "/conferences?"+params).set("verb" , "get").send().then(function(resp){
-                    scope.confs = resp.data;
-                } , function(err){});
-            }
-        }else {}
-    }
+    network.set("url" , "/conference_tasks").set("verb" , "get").send().then(function(response){
+        scope.confs_tasks = response.data;
+    }).catch(function(error){
+        
+    }).finally(function(){
+        console.log("DONE!!!!")
+    });
     
     scope.export_conference = function(id){
         network.set("url" , "/conference_sheet?conference_id="+id).set("verb" , "get").send().then(function(response){
